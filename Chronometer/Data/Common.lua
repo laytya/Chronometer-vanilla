0ed7e154b489f176e4f6da80b079f01b49c93ef6
LaYt <laytya@gmail.com>
2019-01-13 01:43:51 +0700

    v0.13
    - Added visibility configuration of spells in options by @dein0s
    - Added Flurry timer by @dein0s
    - Fixed Fire Vulnerability update mechanics
    - Added some SPD trinkets timers & Crusader proc

---
+++ Chronometer/Data/Common.lua
@@ -0,0 +1,22 @@
+﻿--<< ====================================================================== >>--
+-- Setup Timers                                                               --
+--<< ====================================================================== >>--
+local BS = AceLibrary("Babble-Spell-2.2")
+
+function Chronometer:CommonSetup()
+
+	local _, eclass = UnitClass("player")
+	
+	if eclass == "PALADIN" or eclass == "WARRIOR" or eclass == "ROGUE" or eclass == "SHAMAN" then
+	
+		-- crusader enchant proc http://classicdb.ch/?item=16252
+		self:AddTimer(self.EVENT, BS["Holy Strength"],         	15, 0,1,1, { cr="YELLOW", a=1 , cl="COMMON" })
+		
+	end
+	-- zg casteer trinket http://classicdb.ch/?item=19950
+	self:AddTimer(self.EVENT, BS["Unstable Power"],          	20, 0, 1, 1, { a=1, cr="CYAN", cl="COMMON" })
+	self:AddTimer(self.EVENT, BS["Ephemeral Power"],          	15, 0, 1, 1, { a=1, cr="CYAN", cl="COMMON" })
+	self:AddTimer(self.EVENT, "Mind Quickening",          		20, 0, 1, 1, { a=1, cr="CYAN", cl="COMMON",xn=BS["Critical Mass"] })
+end
+
+table.insert(Chronometer.dataSetup, Chronometer.CommonSetup)
--- Chronometer/Data/Racial.lua
+++ Chronometer/Data/Racial.lua
@@ -8,17 +8,17 @@ function Chronometer:RacialSetup()
 	local lr, race = UnitRace("player")
 	
 	if race == "Dwarf" then
-		self:AddTimer(self.SPELL, BS["Stoneform"],              8, 0,1,1)
+		self:AddTimer(self.SPELL, BS["Stoneform"],              8, 0,1,1, { cl="RACIAL" })
 	elseif race == "Human" then
-		self:AddTimer(self.SPELL, BS["Perception"],             20, 0,1,1)
+		self:AddTimer(self.SPELL, BS["Perception"],             20, 0,1,1, { cl="RACIAL" })
 	elseif race == "Orc" then
-		self:AddTimer(self.SPELL, BS["Blood Fury"],             15, 0,1,1)
+		self:AddTimer(self.SPELL, BS["Blood Fury"],             15, 0,1,1, { cl="RACIAL" })
 	elseif race == "Tauren" then
-		self:AddTimer(self.SPELL, BS["War Stomp"],              2, 0,0,0)
+		self:AddTimer(self.SPELL, BS["War Stomp"],              2, 0,0,0, { cl="RACIAL" })
 	elseif race == "Troll" then
-		self:AddTimer(self.SPELL, BS["Berserking"],             10, 0,1,1)
+		self:AddTimer(self.SPELL, BS["Berserking"],             10, 0,1,1, { cl="RACIAL" })
 	elseif race == "Scourge" then
-		self:AddTimer(self.SPELL, BS["Will of the Forsaken"],   5, 0,1,1)
+		self:AddTimer(self.SPELL, BS["Will of the Forsaken"],   5, 0,1,1, { cl="RACIAL" })
 	end
 	
 end
