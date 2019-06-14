e9e647e0d59425f2c562b5ea86f9534c1aa9d07e
LaYt <laytya@gmail.com>
2019-06-14 10:33:03 +0700

    - Added more rank numbers

--- Chronometer/Core/Chronometer.lua
+++ Chronometer/Core/Chronometer.lua
@@ -65,7 +65,7 @@ local defaults = {
 Chronometer:RegisterDefaults('profile', defaults)
 
 local options
-local latins = {I=1, II=2, III=3, IV=4, V=5, VI=6} 
+local latins = {I=1, II=2, III=3, IV=4, V=5, VI=6, VII = 7, VIII = 8, IX = 9, X = 10, XI = 11, XII = 12, XIII = 13, XIV = 14} 
 
 --------------------- FOR DEBUG ----------------------------
 local join = function (list, separator)
